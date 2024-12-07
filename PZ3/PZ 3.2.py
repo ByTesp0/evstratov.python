c3e7c8708537a46f66fc54073162adc4cf2977c8
ByTesp0 <evstratov1710@gmail.com>
2024-12-07 15:21:25 +0300

    ПЗ 3 основное задание

---
+++ PZ3/PZ 3.2.py
@@ -0,0 +1,34 @@
+# Найти длину отрезка в метрах.
+
+user_num = input('Введите число от 1 до 5: ')
+
+while type(user_num) != int:
+    try:
+        user_num = int(user_num)
+    except ValueError:
+        print('Введено неверное значение: ')
+
+user_measure = input('Введите длину отрезка: ')
+
+while type(user_measure) != float:
+    try:
+        user_measure = float(user_measure)
+    except ValueError:
+        print('Введено неверное значение: ')
+
+if user_num == 1:
+    user_measure = user_measure / 10
+    print(user_measure)
+elif user_num == 2:
+    user_measure = user_measure * 1000
+    print(user_measure)
+elif user_num == 3:
+    print(user_measure)
+elif user_num == 4:
+    user_measure = user_measure / 1000
+    print(user_measure)
+elif user_num == 5:
+    user_measure = user_measure / 100
+    print(user_measure)
+else:
+    print('Введено неверное число')
