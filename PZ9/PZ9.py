dc544f3d4ebf3dfd1f2c2557c2b539f6e9d433fb
ByTesp0 <evstratov1710@gmail.com>
2025-01-25 14:25:03 +0300

    ПЗ 9

--- PZ9/PZ9.py
+++ PZ9/PZ9.py
@@ -6,13 +6,24 @@
 a = 'апельсины 45 991 63 100 12 яблоки 13 47 26 0 16'
 lst_a = a.split()
 
-num1 = lst_a[0]
-num2 = lst_a[1:6]
-num3 = lst_a[6]
-num4 = lst_a[7:]
+print(lst_a)
 
-orgs = dict.fromkeys([num1], num2)
-apls = dict.fromkeys([num3], num4)
-print(orgs)
-print(apls)
+orgs = dict.fromkeys([lst_a[0]], lst_a[1:6])
+apls = dict.fromkeys([lst_a[6]], lst_a[7:])
 
+c = lst_a[0]
+d = lst_a[6]
+def funkc(a, b):
+    nums = a.get(b)
+    lst = []
+    for i in range(len(nums)):
+        lst.append(int(nums[i]))
+        print(lst)
+    b = 0
+    for i in lst:
+        b = (b + i)
+    b = b/len(lst)
+    print('среднее значение продукции за 5 дней:', b)
+
+funkc(orgs, c)
+funkc(apls, d)
